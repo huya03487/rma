63bc966b687abb45b8928405f3e1b61f65a2bfa7
Khuong Kieu <khuong1503@gmail.com>
2014-07-31 17:48:51 +0700

    add file generate-user

---
+++ source/templates/generate-user.php
@@ -0,0 +1,35 @@
+<form action="" name="form-newuser" method="POST" id="form-newuser" class="">
+	<div id="new-user">
+		<div id="new-user-title" class="title-form">
+			<h1>Create new user</h1>
+		</div>
+		<div clas="new-user-input">
+			<label>Username</label>
+			<input type="input" name="user_name" id="user_name" class="">
+		</div>
+		<div clas="new-user-input">
+			<label>Password</label>
+			<input type="input" name="user_pass" id="user_pass" class="">
+		</div>
+		<div clas="new-user-input">
+			<label>Confirm Password</label>
+			<input type="input" name="user_repass" id="user_repass" class="">
+		</div>
+		<div clas="new-user-input">
+			<label>Fullname</label>
+			<input type="input" name="user_fullname" id="user_fullname" class="">
+		</div>
+		<div clas="new-user-input">
+			<label>Email</label>
+			<input type="input" name="user_email" id="user_email" class="">
+		</div>
+		<div clas="new-user-input">
+			<label>Phone</label>
+			<input type="input" name="user_phone" id="user_phone" class="">
+		</div>
+		<div id="new-user-action">
+			<input type="submit" value="Save" name="submit_newuser" id="submit_newuser" class="">
+			<input type="reset" value="Reset" id="reset_newuser" class=""> 
+		</div>
+	</div>
+</form>
